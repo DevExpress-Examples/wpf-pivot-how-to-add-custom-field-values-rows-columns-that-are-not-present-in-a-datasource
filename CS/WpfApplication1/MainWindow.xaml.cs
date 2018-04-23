f2fb583c364e79c10de7a681cfccbdcdabcde968
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using CustomDatesPivot;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataTable Table;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dateEdit1.DateTime = DateTime.Now;
+            dateEdit2.DateTime = DateTime.Now.AddMonths(12);
+            Table = CreatePivotTable(100);
+            pivot.DataSource = Table;
+        }
+
+        private static DataTable CreatePivotTable(int RowCount)
+        {
+            Random rnd = new Random();
+
+            DataTable tbl = new DataTable();
+            tbl.Columns.Add("ID", typeof(int));
+            tbl.Columns.Add("Name", typeof(string));
+            tbl.Columns.Add("Checked", typeof(bool));
+            tbl.Columns.Add("Number", typeof(int));
+            tbl.Columns.Add("Value", typeof(decimal));
+            tbl.Columns.Add("AltValue", typeof(decimal));
+            tbl.Columns.Add("Date", typeof(DateTime));
+
+
+            for (int i = 0; i < RowCount; i++)
+            {
+                DataRow row = tbl.Rows.Add(new object[] { i, String.Format("Name{0}", i % 9), i % 2 == 0, rnd.Next(10), rnd.Next(100, 1000), rnd.Next(100, 1000), DateTime.Now.AddDays(rnd.Next(300)) });
+            }
+            return tbl;
+        }
+
+        private void pivot_CustomFieldValueCells(object sender, DevExpress.Xpf.PivotGrid.PivotCustomFieldValueCellsEventArgs e)
+        {
+            int index = pivot.GetRowIndex(new object[] { null });
+            if (index != -1)
+                e.Remove(e.GetCell(false, index));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> DateFields = new List<string>();
+            DateFields.Add("Date");
+            List<DateTime> CustomDates = new List<DateTime>();
+            DateTime date = dateEdit1.DateTime;
+            while (date < dateEdit2.DateTime)
+            {
+                CustomDates.Add(date);
+                switch (comboBox.Text)
+                {
+                    case "Year":
+                        date = date.AddYears(1);
+                        break;
+                    case "Quarter":
+                        date = date.AddMonths(3);
+                        break;
+                    case "Month":
+                        date = date.AddMonths(1);
+                        break;
+                    default:
+                        date = date.AddDays(1);
+                        break;
+                }
+            }
+            pivot.DataSource = new DateDataSourceWrapper(Table.DefaultView, DateFields, CustomDates);
+        }
+    }
+}
---
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -0,0 +1,84 @@
+﻿Imports CustomDatesPivot
+Imports System
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace WpfApplication1
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Private Table As DataTable
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            dateEdit1.DateTime = Date.Now
+            dateEdit2.DateTime = Date.Now.AddMonths(12)
+            Table = CreatePivotTable(100)
+            pivot.DataSource = Table
+        End Sub
+
+        Private Shared Function CreatePivotTable(ByVal RowCount As Integer) As DataTable
+            Dim rnd As New Random()
+
+            Dim tbl As New DataTable()
+            tbl.Columns.Add("ID", GetType(Integer))
+            tbl.Columns.Add("Name", GetType(String))
+            tbl.Columns.Add("Checked", GetType(Boolean))
+            tbl.Columns.Add("Number", GetType(Integer))
+            tbl.Columns.Add("Value", GetType(Decimal))
+            tbl.Columns.Add("AltValue", GetType(Decimal))
+            tbl.Columns.Add("Date", GetType(Date))
+
+
+            For i As Integer = 0 To RowCount - 1
+                Dim row As DataRow = tbl.Rows.Add(New Object() { i, String.Format("Name{0}", i Mod 9), i Mod 2 = 0, rnd.Next(10), rnd.Next(100, 1000), rnd.Next(100, 1000), Date.Now.AddDays(rnd.Next(300)) })
+            Next i
+            Return tbl
+        End Function
+
+        Private Sub pivot_CustomFieldValueCells(ByVal sender As Object, ByVal e As DevExpress.Xpf.PivotGrid.PivotCustomFieldValueCellsEventArgs)
+            Dim index As Integer = pivot.GetRowIndex(New Object() { Nothing })
+            If index <> -1 Then
+                e.Remove(e.GetCell(False, index))
+            End If
+        End Sub
+
+        Private Sub button_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim DateFields As New List(Of String)()
+            DateFields.Add("Date")
+            Dim CustomDates As New List(Of Date)()
+            Dim [date] As Date = dateEdit1.DateTime
+            Do While [date] < dateEdit2.DateTime
+                CustomDates.Add([date])
+                Select Case comboBox.Text
+                    Case "Year"
+                        [date] = [date].AddYears(1)
+                    Case "Quarter"
+                        [date] = [date].AddMonths(3)
+                    Case "Month"
+                        [date] = [date].AddMonths(1)
+                    Case Else
+                        [date] = [date].AddDays(1)
+                End Select
+            Loop
+            pivot.DataSource = New DateDataSourceWrapper(Table.DefaultView, DateFields, CustomDates)
+        End Sub
+    End Class
+End Namespace
